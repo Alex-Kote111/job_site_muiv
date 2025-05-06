2a96042d213461ef0a03f1e11a7e14031fe5a91d
Alex-Kote111 <taukpe@bk.ru>
2025-05-06 16:14:33 +0300

    Добавлены стили для блоков вакансий
    Добавлена ссылка в виде кнопки на блок
    
    Если вакансий нет, то будет выводиться соответствующее сообщение

--- includes/brief_description_vacancy.php
+++ includes/brief_description_vacancy.php
@@ -1,15 +1,14 @@
 <?php
 require 'connect.php';
 
-$query = "SELECT vacancy_title, education, experience FROM vacancies";
+$query = "SELECT vacancy_title, type_of_employment, experience, salary_from, salary_up_to, before_tax FROM vacancies";
 $result = pg_query($conn, $query);
-
 if (!$result) {
     die("Ошибка запроса: " . pg_last_error());
 }
 
 
-while ($row = pg_fetch_assoc($result)) {
+/* while ($row = pg_fetch_assoc($result)) {
 
     echo $row['vacancy_title'] . " " . $row['education'] . " " . $row['experience'] . "<br>";
-}
+} */
--- public/index.php
+++ public/index.php
@@ -8,12 +8,54 @@ require __DIR__ . '/../includes/brief_description_vacancy.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/styles.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Главная страница</h1>
+    <header>
+        <h1>Вакансии Московского университета С.Ю. Витте</h1>
+    </header>
+    <main>
+        <?php
+        if (pg_num_rows($result) == 0) { ?>
+            <div class="no_vacancies"><?php echo "В данный момент нет открытых вакансий.";  ?></div>
+            <?php
+        } else {
+            while ($row = pg_fetch_assoc($result)) {
+            ?>
+                <div class="vacancy_block">
+                    <div class="vacancy_title"><?php echo $row['vacancy_title'] ?></div>
+                    <div class="vacancy_salary">
+                        <?php
+                        $payment_terms = ""; // условия оплаты
+                        if (is_null($row['salary_from']) && !is_null($row['salary_up_to'])) {
+                            $payment_terms .= "Оплата труда до: " . $row['salary_up_to'] . " ₽ за месяц";
+                        } elseif (!is_null($row['salary_from']) && is_null($row['salary_up_to'])) {
+                            $payment_terms .= "Оплата труда от: " . $row['salary_from'] . " ₽ за месяц";
+                        } elseif (!is_null($row['salary_from']) && !is_null($row['salary_up_to'])) {
+                            $payment_terms .= "Оплата труда от: " . $row['salary_from'] . " до: " . $row['salary_up_to'] . " ₽ за месяц";
+                        }
 
+                        if ($row['before_tax'] === "t" && (!is_null($row['salary_from']) || !is_null($row['salary_up_to']))) {
+                            $payment_terms .= " (до вычета налога)";
+                        }
+                        echo $payment_terms;
+                        ?>
+                    </div>
+                    <div class="vacancy_experience"><?php echo "Опыт: " . $row['experience'] ?></div>
+                    <div class="vacancy_type_of_employment"><?php echo $row['type_of_employment'] ?></div>
+                    <a href="" class="button-style">Подробнее</a>
+                </div>
+        <?php
+            }
+        }
+        ?>
+    </main>
+    <footer>
+        <p>2025 &copy; МУИВ. Все права защищены.</p>
+    </footer>
 </body>
 
 </html>
