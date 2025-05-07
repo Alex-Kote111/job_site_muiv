4cb42e9bc74968338e1fa3123235b30952c96da4
Alex-Kote111 <taukpe@bk.ru>
2025-05-08 01:41:28 +0300

    Добавлена возможность удаления вакансии из 1С

---
+++ includes/delete_vacancy.php
@@ -0,0 +1,21 @@
+<?php
+require 'connect.php';
+
+if (!array_key_exists('vacancy_number', $_GET) || !array_key_exists('HTTP_APPLICATION', $_SERVER)) {
+    http_response_code(404);
+    die();
+}
+
+if ($_SERVER['HTTP_APPLICATION'] !== "1C_recruitment") {
+    http_response_code(404);
+    die();
+}
+$vacancy_number = $_GET['vacancy_number'];
+// Удаляем вакансию
+$query = "DELETE FROM vacancies WHERE vacancy_number = $1";
+$result = pg_query_params($conn, $query, array($vacancy_number));
+
+if (!$result) {
+    http_response_code(400); // не удалось удалить вакансию
+    die();
+}
